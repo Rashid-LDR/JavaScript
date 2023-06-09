bf9395ec4386c65f457db62cb2df36bd443625a4
Rashid <mrashidali51@gmail.com>
2023-06-09 01:20:33 -0700

    Calculate Object is Added

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var Car={
-    name:"Rashid",
-    age:27,
-    calulate:()=>{
-        return this.name+this.age
+// var Car={
+//     name:"Rashid",
+//     age:27,
+//     calulate:()=>{
+//         return this.name+this.age
+//     }
+// }
+//  var x=Car.calulate()
+
+// console.log(x)
+
+
+const calulate={
+    nam:0,
+    num:0,
+    add:()=>{
+        return this.nam + this.num;
     }
 }
- var x=Car.calulate()
 
-console.log(x)
+this.nam=5;
+this.num=5;
+console.log(calulate.add());
