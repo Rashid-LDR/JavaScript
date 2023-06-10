a196ce91de6daccc1e76acb34af21b4ba81c1642
Rashid <mrashidali51@gmail.com>
2023-06-09 20:58:35 -0700

    Number Operation is performing right Now

--- number.js
+++ number.js
@@ -1,2 +1,20 @@
 let num=new Date("1970-01-01")
-console.log(num);
+// console.log(num);
+
+// Parse Method is Perform 
+
+var x= parseInt('-10');
+// console.log( x);
+
+x=parseInt('aa27');
+// console.log(x);
+if(x>=0 || x<0)
+{
+    // console.log("Number is ",x);
+}
+else{
+    // console.log("Number is Not ");
+}
+
+x=parseFloat('3.89');
+console.log(x);
