e4230385c247db06f77ef84acd75000d98510732
Rashid <mrashidali51@gmail.com>
2023-06-09 20:44:46 -0700

    String Method is added

---
+++ stringmethod.js
@@ -0,0 +1,19 @@
+let str="this is my first string";
+
+// find Length of String
+let length=str.length;
+// console.log(length);
+
+
+// String Slicing 
+
+let sliceing =str.slice(5,18)
+// console.log(sliceing);
+
+
+let searchString=str.indexOf('first');
+// console.log(searchString);
+let lastString=str.lastIndexOf("first");
+
+
+console.log(lastString);
